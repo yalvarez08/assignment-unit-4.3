7f3fdd11110ac87ac026e3271bd6e088a0eae518
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-01-07 13:19:04 -0500

    added to cart js file

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,8 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+//global variable 'basket' with empty array
+let basket = [];
 
 
 
